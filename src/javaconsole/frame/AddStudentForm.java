24846900ff5fe8faf1b4269c9bf82205222a18a9
Dongvud00422 <dongvdd00422@fpt.com.vn>
2017-09-28 01:49:25 +0700

    M

--- src/javaconsole/frame/AddStudentForm.java
+++ src/javaconsole/frame/AddStudentForm.java
@@ -9,24 +9,16 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
+import java.util.HashMap;
+import javaconsole.entity.Student;
+import javaconsole.model.StudentModel;
+import javaconsole.vaildate.VaildateInputData;
 import javax.swing.JButton;
-import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.text.DateFormatter;
-import javax.swing.text.MaskFormatter;
 
 /**
  *
@@ -35,12 +27,10 @@ import javax.swing.text.MaskFormatter;
 public class AddStudentForm extends JFrame {
 
     public static void main(String[] args) {
-        try {
-            AddStudentForm add = new AddStudentForm();
-            add.setVisible(true);
-        } catch (ParseException ex) {
-            System.out.println(ex.getMessage());
-        }
+
+        AddStudentForm add = new AddStudentForm();
+        add.setVisible(true);
+
     }
 
     private JPanel background;
@@ -51,6 +41,15 @@ public class AddStudentForm extends JFrame {
     private JLabel lblPhone;
     private JLabel lblRollNumber;
     private JLabel lblClassName;
+
+    private JLabel lblTotalMessage;
+    private JLabel lblNameMessage;
+    private JLabel lblBirthdayMessage;
+    private JLabel lblEmailMessage;
+    private JLabel lblPhonemessage;
+    private JLabel lblRollNumberMessage;
+    private JLabel lblClassNameMessage;
+
     private JTextField txtId;
     private JTextField txtName;
     private JTextField txtBirthday;
@@ -62,89 +61,106 @@ public class AddStudentForm extends JFrame {
     private JButton btnReset;
     private JButton btnCancel;
 
-    public AddStudentForm() throws ParseException {
-        this.setSize(950, 650);
-        this.setResizable(false);
-        this.setLocationRelativeTo(null);
+    public AddStudentForm() {
+       
 
-        try {
-            this.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("/Users/dongvu/NetBeansProjects/javaconsole/src/javaconsole/frame/background.png")))));
-        } catch (IOException ex) {
-            System.out.println("Set background Failed " + ex.getMessage());
-        }
+       
 
         this.background = new JPanel();
         this.background.setBackground(new Color(0, 0, 0, 80));
         this.background.setBounds(450, 40, 450, 550);
         this.background.setLayout(null);
 
+        this.lblTotalMessage = new JLabel();
+        this.lblTotalMessage.setBounds(10, 115, 420, 20);
+
         this.lblId = new JLabel("Student ID: ");
-        this.lblId.setBounds(10, 240, 90, 30);
+        this.lblId.setBounds(10, 145, 90, 30);
         this.lblId.setForeground(Color.white);
         this.lblId.setFont(new Font("Arial", 3, 12));
 
         this.txtId = new JTextField();
-        this.txtId.setBounds(100, 240, 330, 30);
+        this.txtId.setBounds(100, 145, 330, 30);
         this.txtId.setEditable(false);
-        String strId = String.valueOf(System.currentTimeMillis());
-        this.txtId.setText(strId);
+        this.txtId.setText(String.valueOf(System.currentTimeMillis()));
 
         this.lblName = new JLabel("Name: ");
-        this.lblName.setBounds(10, 280, 90, 30);
+        this.lblName.setBounds(10, 200, 90, 30);
         this.lblName.setForeground(Color.white);
         this.lblName.setFont(new Font("Arial", 3, 12));
 
         this.txtName = new JTextField();
-        this.txtName.setBounds(100, 280, 330, 30);
+        this.txtName.setBounds(100, 200, 330, 30);
 
+        this.lblNameMessage = new JLabel();
+        this.lblNameMessage.setBounds(100, 235, 330, 10);
+        this.lblNameMessage.setOpaque(true);
+        this.lblNameMessage.setBackground(new Color(0, 0, 0, 0));
 
         this.lblBirthday = new JLabel("Birthday: ");
-        this.lblBirthday.setBounds(10, 320, 90, 30);
+        this.lblBirthday.setBounds(10, 250, 90, 30);
         this.lblBirthday.setForeground(Color.white);
         this.lblBirthday.setFont(new Font("Arial", 3, 12));
 
-        this.txtBirthday = new JFormattedTextField(new MaskFormatter("##/##/####"));
-       
-        
-        this.txtBirthday.setBounds(100, 320, 330, 30);
+        this.txtBirthday = new JTextField();
+        this.txtBirthday.setBounds(100, 250, 330, 30);
+
+        this.lblBirthdayMessage = new JLabel();
+        this.lblBirthdayMessage.setBounds(100, 285, 330, 10);
+        this.lblBirthdayMessage.setOpaque(true);
+        this.lblBirthdayMessage.setBackground(new Color(0, 0, 0, 0));
 
         this.lblEmail = new JLabel("Email: ");
-        this.lblEmail.setBounds(10, 355, 90, 30);
+        this.lblEmail.setBounds(10, 300, 90, 30);
         this.lblEmail.setForeground(Color.white);
         this.lblEmail.setFont(new Font("Arial", 3, 12));
 
         this.txtEmail = new JTextField();
-        this.txtEmail.setBounds(100, 355, 330, 30);
+        this.txtEmail.setBounds(100, 300, 330, 30);
 
+        this.lblEmailMessage = new JLabel();
+        this.lblEmailMessage.setBounds(100, 335, 330, 10);
+        this.lblEmailMessage.setOpaque(true);
+        this.lblEmailMessage.setBackground(new Color(0, 0, 0, 0));
 
         this.lblPhone = new JLabel("Phone: ");
-        this.lblPhone.setBounds(10, 390, 90, 30);
+        this.lblPhone.setBounds(10, 350, 90, 30);
         this.lblPhone.setForeground(Color.white);
         this.lblPhone.setFont(new Font("Arial", 3, 12));
 
-        
-        
-        this.txtPhone = new JFormattedTextField(new MaskFormatter("###########"));
-        this.txtPhone.setBounds(100, 390, 330, 30);
-     
+        this.txtPhone = new JTextField();
+        this.txtPhone.setBounds(100, 350, 330, 30);
+
+        this.lblPhonemessage = new JLabel();
+        this.lblPhonemessage.setBounds(100, 385, 330, 10);
+        this.lblPhonemessage.setOpaque(true);
+        this.lblPhonemessage.setBackground(new Color(0, 0, 0, 0));
 
         this.lblRollNumber = new JLabel("Roll-number: ");
-        this.lblRollNumber.setBounds(10, 425, 90, 30);
+        this.lblRollNumber.setBounds(10, 400, 90, 30);
         this.lblRollNumber.setForeground(Color.white);
         this.lblRollNumber.setFont(new Font("Arial", 3, 12));
 
         this.txtRollNumber = new JTextField();
-        this.txtRollNumber.setBounds(100, 425, 330, 30);
-  
+        this.txtRollNumber.setBounds(100, 400, 330, 30);
+
+        this.lblRollNumberMessage = new JLabel();
+        this.lblRollNumberMessage.setBounds(100, 435, 330, 10);
+        this.lblRollNumberMessage.setOpaque(true);
+        this.lblRollNumberMessage.setBackground(new Color(0, 0, 0, 0));
 
         this.lblClassName = new JLabel("Class-name: ");
-        this.lblClassName.setBounds(10, 460, 90, 30);
+        this.lblClassName.setBounds(10, 450, 90, 30);
         this.lblClassName.setForeground(Color.white);
         this.lblClassName.setFont(new Font("Arial", 3, 12));
 
         this.txtClassName = new JTextField();
-        this.txtClassName.setBounds(100, 460, 330, 30);
+        this.txtClassName.setBounds(100, 450, 330, 30);
 
+        this.lblClassNameMessage = new JLabel();
+        this.lblClassNameMessage.setBounds(100, 485, 330, 10);
+        this.lblClassNameMessage.setOpaque(true);
+        this.lblClassNameMessage.setBackground(new Color(0, 0, 0, 0));
 
         this.btnSubmit = new JButton("Submit");
         this.btnSubmit.setBounds(100, 500, 80, 30);
@@ -153,10 +169,10 @@ public class AddStudentForm extends JFrame {
         this.btnReset = new JButton("Reset");
         this.btnReset.setBounds(185, 500, 70, 30);
         this.btnReset.addActionListener(new resetHandle());
-        
-        this.btnCancel = new JButton("Cancel");
-        this.btnCancel.setBounds(360, 500, 70, 30);
-        this.btnCancel.addActionListener(new resetHandle());
+
+        this.btnCancel = new JButton("View all");
+        this.btnCancel.setBounds(340, 500, 90, 30);
+      //  this.btnCancel.addActionListener();
 
         this.background.add(lblId);
         this.background.add(txtId);
@@ -175,33 +191,57 @@ public class AddStudentForm extends JFrame {
         this.background.add(btnSubmit);
         this.background.add(btnReset);
         this.background.add(btnCancel);
+        this.background.add(lblTotalMessage);
+        this.background.add(lblNameMessage);
+        this.background.add(lblEmailMessage);
+        this.background.add(lblPhonemessage);
+        this.background.add(lblRollNumberMessage);
+        this.background.add(lblClassNameMessage);
+        this.background.add(lblBirthdayMessage);
 
-        this.add(this.background);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
     }
 
     class loginHandle implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            StringBuilder alert = new StringBuilder();
-            alert.append("Are you want to add this student ?\n");
-            alert.append("Student Id: ");
-            alert.append(txtId.getText());
-            alert.append("\nName: ");
-            alert.append(txtName.getText());
-            alert.append("\nBirthday: ");
-            alert.append(txtBirthday.getText());
-            alert.append("\nEmail: ");
-            alert.append(txtEmail.getText());
-            alert.append("\nPhone: ");
-            alert.append(txtPhone.getText());
-            alert.append("\nRoll-number: ");
-            alert.append(txtRollNumber.getText());
-            alert.append("\nClass-name: ");
-            alert.append(txtClassName.getText());
-            
-            JOptionPane.showConfirmDialog(rootPane, alert.toString());
+            Student student = new Student(Long.parseLong(txtId.getText()), txtName.getText(), txtBirthday.getText(), txtPhone.getText(), txtEmail.getText(), txtClassName.getText(), txtRollNumber.getText());
+            HashMap<String, String> errors = new VaildateInputData().errors(student);
+            resetlblErrors();
+            if (errors.isEmpty()) {
+                StringBuilder alert = new StringBuilder();
+                alert.append("Are you want to add this student ?\n");
+                alert.append("Student Id: ");
+                alert.append(txtId.getText());
+                alert.append("\nName: ");
+                alert.append(txtName.getText());
+                alert.append("\nBirthday: ");
+                alert.append(txtBirthday.getText());
+                alert.append("\nEmail: ");
+                alert.append(txtEmail.getText());
+                alert.append("\nPhone: ");
+                alert.append(txtPhone.getText());
+                alert.append("\nRoll-number: ");
+                alert.append(txtRollNumber.getText());
+                alert.append("\nClass-name: ");
+                alert.append(txtClassName.getText());
+
+                int result = JOptionPane.showConfirmDialog(rootPane, alert.toString());
+                if (result == 0) {
+                    // Thực hiện insert student khi chọn yes.
+                    StudentModel studentModel = new StudentModel();
+                    studentModel.insert(student);
+                } else if (result == 1) {
+                    // Thực hiện reset các field khi chọn no.
+                    resetField();
+                }
+
+            } else {
+                showErrors(errors);
+
+            }
+
         }
     }
 
@@ -209,16 +249,64 @@ public class AddStudentForm extends JFrame {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            txtId.setText(String.valueOf(System.currentTimeMillis()));
-            txtName.setText("");
-            txtBirthday.setText("");
-            txtEmail.setText("");
-            txtPhone.setText("");
-            txtRollNumber.setText("");
-            txtClassName.setText("");
+            resetField();
+            resetlblErrors();
 
         }
 
     }
+
+
+    private void resetlblErrors() {
+        lblTotalMessage.setText("");
+        lblNameMessage.setText("");
+        lblBirthdayMessage.setText("");
+        lblEmailMessage.setText("");
+        lblPhonemessage.setText("");
+        lblRollNumberMessage.setText("");
+        lblClassNameMessage.setText("");
+    }
+
+    private void resetField() {
+        txtId.setText(String.valueOf(System.currentTimeMillis()));
+        txtName.setText("");
+        txtBirthday.setText("");
+        txtEmail.setText("");
+        txtPhone.setText("");
+        txtRollNumber.setText("");
+        txtClassName.setText("");
+    }
+
+    private void showErrors(HashMap<String, String> errors) {
+        lblTotalMessage.setForeground(Color.red);
+        lblTotalMessage.setText("* Invalid student information, please change the errors !!");
+       // lblTotalMessage.setOpaque(true);
+        lblTotalMessage.setBackground(new Color(0, 0, 0, 0));
+        if (errors.containsKey("txtName")) {
+            lblNameMessage.setText(errors.get("txtName"));
+            lblNameMessage.setForeground(Color.red);
+        }
+        if (errors.containsKey("txtBirthday")) {
+            lblBirthdayMessage.setText(errors.get("txtBirthday"));
+            lblBirthdayMessage.setForeground(Color.red);
+        }
+        if (errors.containsKey("txtEmail")) {
+            lblEmailMessage.setText(errors.get("txtEmail"));
+            lblEmailMessage.setForeground(Color.red);
+        }
+        if (errors.containsKey("txtPhone")) {
+            lblPhonemessage.setText(errors.get("txtPhone"));
+            lblPhonemessage.setForeground(Color.red);
+        }
+        if (errors.containsKey("txtRollNumber")) {
+            lblRollNumberMessage.setText(errors.get("txtRollNumber"));
+            lblRollNumberMessage.setForeground(Color.red);
+        }
+        if (errors.containsKey("txtClassName")) {
+            lblClassNameMessage.setText(errors.get("txtClassName"));
+            lblClassNameMessage.setForeground(Color.red);
+        }
+    }
+ 
    
 }
